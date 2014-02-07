6254ea04af86233c30d5833e6d53d0ede412457d
jchansen <jason@Jasons-MacBook-Pro.local>
2014-02-06 19:41:10 -0700

    added requirejs grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options: {
+          mainConfigFile: "public/app/main.js",
+          appDir: 'public/',
+          baseUrl: 'app',
+          dir: "public_compiled",
+          removeCombined: true,
+          optimize: 'none',
+          modules: [
+            {
+              name: "main"
+            }
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  grunt.registerTask('default', ['requirejs']);
+};
