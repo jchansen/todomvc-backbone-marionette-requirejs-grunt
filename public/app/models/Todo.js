e3876a1d5e1f0c307fc6c5893bcef19c3d71b634
jchansen <jason@Jasons-MacBook-Pro.local>
2014-03-17 19:21:57 -0700

    setting idAttribute to conform to mongodb standard

--- public/app/models/Todo.js
+++ public/app/models/Todo.js
@@ -1,29 +1,30 @@
 /*global define */
 
 define([
-	'backbone',
-	'localStorage'
+  'backbone',
+  'localStorage'
 ], function (Backbone) {
-	'use strict';
+  'use strict';
 
-	return Backbone.Model.extend({
-		urlRoot: "http://todomvc-api.herokuapp.com/api/todos",
+  return Backbone.Model.extend({
+    urlRoot: "http://todomvc-api.herokuapp.com/api/todos",
+    idAttribute: "_id",
 
-		defaults: {
-			title: '',
-			completed: false,
-			created: 0
-		},
+    defaults: {
+      title: '',
+      completed: false,
+      created: 0
+    },
 
-		initialize: function () {
-			if (this.isNew()) {
-				this.set('created', Date.now());
-			}
-		},
+    initialize: function () {
+      if (this.isNew()) {
+        this.set('created', Date.now());
+      }
+    },
 
-		toggle: function () {
-			return this.set('completed', !this.get('completed'));
-		}
-	});
+    toggle: function () {
+      return this.set('completed', !this.get('completed'));
+    }
+  });
 });
 
