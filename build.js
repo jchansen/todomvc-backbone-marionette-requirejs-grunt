74b74574df767f0f1fe5738cd75f03ea41f46071
jchansen <jason@Jasons-MacBook-Pro.local>
2014-03-17 19:09:48 -0700

    added build file for r.js

---
+++ build.js
@@ -0,0 +1,13 @@
+({
+  appDir: 'public',
+  baseUrl: 'app',
+  mainConfigFile: "public/app/main.js",
+  dir: "public_compiled",
+  removeCombined: true,
+  optimize: 'none',
+  modules: [
+  {
+    name: "main"
+  }
+  ]
+})
