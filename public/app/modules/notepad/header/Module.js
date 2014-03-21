296cc8d929cba8d62c52de58652f0aff66a4278f
jchansen <jason@Jasons-MacBook-Pro.local>
2014-03-20 18:10:42 -0700

    collection no longer needed in header

--- public/app/modules/notepad/header/Module.js
+++ public/app/modules/notepad/header/Module.js
@@ -9,17 +9,7 @@ define(
     'use strict';
 
     return Conductor.ItemViewModule.extend({
-      view: View,
-
-      data: {
-        collection: function () {
-          var defer = Q.defer();
-          app.Repositories.Todos().getAll().done(function (todos) {
-            defer.resolve(todos);
-          });
-          return defer.promise;
-        }
-      }
+      view: View
     });
 
   });
