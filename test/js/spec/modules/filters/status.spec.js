d3e078f9a87d74cec17089acfb89d02e3722ad06
jchansen <jason@Jasons-MacBook-Pro.local>
2014-03-23 18:01:13 -0700

    added tests for filter events

--- test/js/spec/modules/filters/status.spec.js
+++ test/js/spec/modules/filters/status.spec.js
@@ -48,7 +48,7 @@ define(
           });
         });
 
-        it("should not display filters", function(){
+        it("should not be visible", function(){
           var $el = app.appRegion.$el;
           expect($el.find('div')[0].style['display']).to.equal("none");
         });
@@ -89,6 +89,15 @@ define(
           });
         });
 
+        afterEach(function(){
+          app.vent.off();
+        });
+
+        it("should be visible", function(){
+          var $el = app.appRegion.$el;
+          expect($el.find('div')[0].style['display']).to.not.equal("none");
+        });
+
         it("should have three filters", function(){
           var $el = app.appRegion.$el;
           var filters = $el.find('.filters a');
@@ -96,6 +105,53 @@ define(
           expect(filters[1].outerHTML).to.equal('<a href="#active">Active</a>');
           expect(filters[2].outerHTML).to.equal('<a href="#completed">Completed</a>');
         });
+
+        it("should send event when the 'all' filter is clicked", function(done){
+          var $el = app.appRegion.$el;
+          var filter = $el.find('li a')[0];
+
+          // check for the correct event
+          app.vent.on('todoList:filter', function(e){
+            expect(e).to.equal('');
+            done();
+          });
+
+          // click the filter
+          var e = $.Event("click");
+          $(filter).trigger(e);
+        });
+
+        it("should send event when the 'active' filter is clicked", function(done){
+          var $el = app.appRegion.$el;
+          var filter = $el.find('li a')[1];
+
+          // check for the correct event
+          app.vent.on('todoList:filter', function(e){
+            expect(e).to.equal('active');
+            done();
+          });
+
+          // click the filter
+          var e = $.Event("click");
+          $(filter).trigger(e);
+        });
+
+        it("should send event when the 'completed' filter is clicked", function(done){
+          var $el = app.appRegion.$el;
+          var filter = $el.find('li a')[2];
+
+          // check for the correct event
+          app.vent.on('todoList:filter', function(e){
+            expect(e).to.equal('completed');
+            done();
+          });
+
+          // click the filter
+          var e = $.Event("click");
+          $(filter).trigger(e);
+        });
+
+
 
       });
 
