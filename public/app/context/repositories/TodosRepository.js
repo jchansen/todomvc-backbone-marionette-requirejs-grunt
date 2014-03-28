bcd3bb72b50e6a353bcd01844c7841d461961240
jchansen <jason@Jasons-MacBook-Pro.local>
2014-03-27 22:35:23 -0700

    deleted unnecessary content

--- public/app/context/repositories/TodosRepository.js
+++ public/app/context/repositories/TodosRepository.js
@@ -10,7 +10,6 @@ define(
     var Repository = BackboneRepository.extend({
       _collectionType: TodoList,
       _logger: null,
-      isPage: true,
 
       initialize: function (options) {
         BackboneRepository.prototype.initialize.apply(this, arguments);
@@ -21,37 +20,6 @@ define(
           description: function(model){
             return model.get('title');
           }
-        });
-      },
-
-      getPage: function(options){
-        options = options || {};
-        _.defaults(options, {page: 1, pageSize: 5});
-        var defer = Q.defer();
-        this._getPage(options, function(result){
-          defer.resolve(result);
-        });
-        return defer.promise;
-      },
-
-      _getPage: function(options, callback){
-        var self = this;
-        this.getAll().done(function(todos){
-          var skip = (options.page - 1)*options.pageSize;
-          var take = options.pageSize;
-          var result = self._collection
-                            .chain()
-                            .rest(skip)
-                            .first(take || self._collection.length - skip)
-                            .value();
-          var response = new self._collectionType(result);
-          callback(response);
-
-//          return _(array)
-//            .chain()
-//            .rest(skip)
-//            .first(take || array.length - skip)
-//            .value();
         });
       }
 
